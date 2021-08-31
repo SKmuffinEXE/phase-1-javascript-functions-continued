f1fb8ccd6f3e5965f7839409b849e650d1883c9e
David <DEvangelista54@gmail.com>
2021-08-31 16:31:35 -0400

    commit tree d7927f6dae970b28606c9935e3be234911c85e62

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+function saturdayFun(activity, start = 'This Saturday, I want to'){
+    const defAct = 'roller-skate';
+    if(activity === undefined){
+        activity= defAct
+    }
+    return `${start} ${activity}!`
+}
+
+//result  msg = `This Saturday, I want to ${activity}!`;
+
+ function mondayWork(activity, start = 'This Monday, I will'){
+    const defAct = 'go to the office';
+    if(activity === undefined){
+        activity= defAct
+    }
+    return `${start} ${activity}.`
+ }
+
+function wrapAdjective(special = '*', start = "You are") {
+
+    const innerFunction = function (end){
+        return `${start} ${special}${end}${special}!` 
+    }
+    return innerFunction;
+}
+
+// wrapAdjective("%")("a dedicated programmer"); 
+//=> "You are %a dedicated programmer%!"
